dcd33155b7ac71c84778d0fe76621e1a3533cf5d
dhruv <dhruvslashstar@gmail.com>
2025-12-02 17:08:39 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,3 @@
 export default function handler(req, res) {
   res.status(200).json({ text: "Hello" });
 }
-
-export default function handler(req, res) {
-    const email = req.body.email;
-    // Then save email to your database, etc...
-  }
-  
